68dbdd9ff980f7c6a6de8509219cf5f92135b21f
ellina-s <ellina.sergeyeva@gmail.com>
2015-10-02 13:24:04 -0700

    Fixed dependencies and related issues

--- app/src/main/java/elli/nutritionapp/MainActivity.java
+++ app/src/main/java/elli/nutritionapp/MainActivity.java
@@ -2,13 +2,11 @@ package elli.nutritionapp;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
     private static final int TABS_COUNT = 2;
     private ViewPager mViewPager;
