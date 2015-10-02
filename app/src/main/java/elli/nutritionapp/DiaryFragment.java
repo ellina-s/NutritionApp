186af883dd805f5e31d1f493e974a77be3e71e66
ellina-s <ellina.sergeyeva@gmail.com>
2015-10-02 12:17:14 -0700

    Added basic implementation and layout of fragments

--- app/src/main/java/elli/nutritionapp/DiaryFragment.java
+++ app/src/main/java/elli/nutritionapp/DiaryFragment.java
@@ -1,9 +1,21 @@
 package elli.nutritionapp;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Ellina on 02-Oct-15.
  */
 public class DiaryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_diary, container, false);
+        return rootView;
+    }
 }
