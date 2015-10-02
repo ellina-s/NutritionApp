c49515aedde4d99e9a90b9eea1d4dc9b9f632e52
ellina-s <ellina.sergeyeva@gmail.com>
2015-10-02 06:00:20 -0700

    Implemented switching between the tabs in ViewPager

--- app/src/main/java/elli/nutritionapp/MainActivity.java
+++ app/src/main/java/elli/nutritionapp/MainActivity.java
@@ -2,6 +2,7 @@ package elli.nutritionapp;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
 
     private static final int TABS_COUNT = 3;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +19,19 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
         setContentView(R.layout.activity_main);
 
         final ActionBar actionBar = getActionBar();
+        mViewPager = (ViewPager) findViewById(R.id.pager);
 
         // Enable tabs to be displayed in the action bar
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                // When the user swipes between the tabs, set the given tab selected.
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
 
         // Add tabs to the action bar
         for(int i = 0; i < TABS_COUNT; i++ ){
@@ -31,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        // When the user selects a given tab, switch to that tab in the ViewPager
+        mViewPager.setCurrentItem(tab.getPosition());
     }
 
     @Override
