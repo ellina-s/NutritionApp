1f7e3370b024ba84415d5ff9642117d1a91cf2db
ellina-s <ellina.sergeyeva@gmail.com>
2015-10-11 23:59:37 -0700

    Implemented onClickListener in the Diary fragment to handle a button click in the fragment rather than in the Main Activiity

--- app/src/main/java/elli/nutritionapp/DiaryFragment.java
+++ app/src/main/java/elli/nutritionapp/DiaryFragment.java
@@ -2,20 +2,34 @@ package elli.nutritionapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Ellina on 02-Oct-15.
  */
 public class DiaryFragment extends Fragment {
 
+    private static final String TAG = "Diary";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_diary, container, false);
+
+        Button button = (Button) rootView.findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "User clicked the Add button");
+            }
+        });
+
         return rootView;
     }
 }
