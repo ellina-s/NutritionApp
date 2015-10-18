246e01b5b6b8b412ca27b69a9989287fcd9022fb
ellina-s <ellina.sergeyeva@gmail.com>
2015-10-17 23:59:53 -0700

    Added a reference for the Notepad sample app
    Sample code was used for reference in creating and managing a SQLite
    database in an Android app.

--- app/src/main/java/elli/nutritionapp/NutritionDbHelper.java
+++ app/src/main/java/elli/nutritionapp/NutritionDbHelper.java
@@ -7,6 +7,11 @@ import android.content.Context;
  * for the Nutrition app. The Servings table contains information
  * on the amount of servings of vegetables and fruit, grains, milk,
  * and meat products and alternatives consumed per day.
+ *
+ * Note: Portions of this file are based on the Notepad sample app provided in the Notepad Tutorial.
+ * Copyright (C) 2008 Google Inc.
+ * Licensed under the Apache 2.0 License available at http://www.apache.org/licenses/LICENSE-2.0
+ * The Notepad tutorial is available at http://developer.android.com/training/notepad/index.html
  */
 public class NutritionDbHelper {
 
